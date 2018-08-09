7a6e542e118bc24b4f855f95966e56ac3e827a14
Amir Naghibi <amirmahdi76@gmail.com>
2018-08-08 22:15:25 -0700

    implemented seat availablity logic

---
+++ course_scrapper.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import send_mail
+
+ubc_econ_102 = "https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=ECON&course=102&section=008"
+page = requests.get(ubc_econ_102)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#print(soup.prettify())
+while True:
+    if soup.find_all('strong')[4].get_text()!='0':
+        send_mail.send_registration_email()
+    #print(soup.find_all('strong'))
+    print(soup.find_all('strong')[4].get_text())
+    sleep(60)
