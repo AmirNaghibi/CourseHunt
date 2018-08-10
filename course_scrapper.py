d3efc3256c5549ec23c9cfe09f48c0654d5655f1
Amir Naghibi <amirmahdi76@gmail.com>
2018-08-09 22:59:38 -0700

    Development (#1)
    * parameterized course_scrapper
    
    * added gitignore
    
    * ignore pyc
    
    * added print info (number of seats)
    
    * added datetime

--- course_scrapper.py
+++ course_scrapper.py
@@ -2,9 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 import send_mail
+import datetime
 
-ubc_econ_102 = "https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=ECON&course=102&section=008"
-page = requests.get(ubc_econ_102)
+'''
+    User Input for
+    Course Name
+    Course Number
+    Section Number
+'''
+course_name = input("enter course name: ")
+course_name = course_name.upper()
+course_number = input("enter course number: ")
+section_number = input("enter course section (ex. 008) ")
+
+course_info_link = "https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept="+course_name+"&course="+course_number+"&section="+section_number
+page = requests.get(course_info_link)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -12,6 +24,8 @@ soup = BeautifulSoup(page.content, 'html.parser')
 while True:
     if soup.find_all('strong')[4].get_text()!='0':
         send_mail.send_registration_email()
+        print("SUCCESS!!\n")
+        exit()
     #print(soup.find_all('strong'))
-    print(soup.find_all('strong')[4].get_text())
-    sleep(30)
+    print(str(datetime.datetime.now())+" "+"number of general seats available: "+soup.find_all('strong')[4].get_text())
+    sleep(3)
